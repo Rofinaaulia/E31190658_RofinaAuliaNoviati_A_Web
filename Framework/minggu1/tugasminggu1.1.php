ff2e496b129fa023a59c72b8944cf2c5d6225dea
Rofina Aulia Noviati <65754779+Rofinaaulia@users.noreply.github.com>
2021-03-15 20:18:34 +0700

    pengumpulan tugas 1

---
+++ Framework/minggu1/tugasminggu1.1.php
@@ -0,0 +1,13 @@
+<?php
+
+// Autor Rofina Aulia Noviati
+
+//deklarasi class
+    class buku {
+        var $judulBuku; //property
+        var $pengarang; //property
+        var $penerbit; //property
+        var $tahun; //property
+        var $cetakan; //property
+    }
+?>
---
+++ Framework/minggu1/tugasminggu1.2.php
@@ -0,0 +1,22 @@
+<?php
+
+//Autor Rofina Aulia Noviati
+
+//pendeklarasian class
+	class kendaraan {
+		var $jumlahRoda;//properties
+		var $warna;//properties
+		var $bahanBakar;//properties
+		var $harga;//properties
+		var $merek;//properties
+		var $tahunPembuatan;//properties
+
+		//function
+		function dapatSubsidi() {
+			if ($this->bahanBakar = 'Premium' && $this->tahunPembuatan < 2005) $subsidi = 'Dapat';
+			else $subsidi = 'Tidak Dapat';
+			return $subsidi;
+		}
+		
+	}
+?>
