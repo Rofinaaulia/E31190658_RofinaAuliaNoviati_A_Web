af9887bbf84ea799656737f041bc2c67b6722f24
Rofina Aulia Noviati <65754779+Rofinaaulia@users.noreply.github.com>
2021-04-12 22:00:37 +0700

    minggu9

---
+++ minggu9/application/models/Hello_model.php
@@ -0,0 +1,12 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Risma Raudlatul","nim"=>"E31190596","gol"=>"A"],
+			["nama"=>"M Tahajjudin A","nim"=>"E31190678","gol"=>"C"],
+			["nama"=>"Nih Latuliya","nim"=>"E31191397","gol"=>"A"] 
+		];
+}
